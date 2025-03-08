023d75dd322724bda774e5458f5b6c32b23e8efe
LaisRochaS <laismaria713@gmail.com>
2025-03-07 22:37:44 -0500

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,3 @@
-// Dynamically update the current year and the last modified date in the footer
-document.getElementById('currentyear').textContent = new Date().getFullYear();
-document.getElementById('lastModified').textContent = "Last modified: " + document.lastModified;
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
